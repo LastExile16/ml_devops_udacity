df05c9c161495f809d614a8995ec36226395b987
Nawras Khudhur <nawras.nzar@gmail.com>
2024-04-22 08:43:49 +0000

    End of EDA

--- proj1_cutomer_churn/churn_library_class.py
+++ proj1_cutomer_churn/churn_library_class.py
@@ -11,47 +11,96 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 
 class ChurnLibrary:
-    def __init__(self, pth):
-        self.df = pd.read_csv(pth, header=0)
+    def __init__(self, file_path):
+        """
+        Initialize ChurnLibrary object.
+
+        Parameters:
+        - file_path (str): Path to the CSV file containing data.
+        """
+        self.df = pd.read_csv(file_path, header=0)
     
-    def plot_and_save_histogram(self, data, xlabel, save_name):
-        '''
-        function to plot and save histogram
-        
-        input: 
-            - data to plot
-            - x-axis label
-            - file name
-        '''
-        
-        fig, ax = plt.subplots()
-        ax.hist(data)
-        ax.set_xlabel(xlabel)
-        ax.set_ylabel('Number of Customers')
+    def _save_plot(self, fig, save_name):
+        """
+        Save the plot to an image file.
+
+        Parameters:
+        - fig (matplotlib.figure.Figure): Figure object to save.
+        - save_name (str): File path to save the image.
+        """
         plt.savefig(save_name)
         plt.close()
     
-    def perform_eda(self):
-        '''
-        perform eda on df and save figures to images folder
-        input:
-                None
+    def _plot_hist_or_bar(self, data, title, save_name, plot_type='hist'):
+        """
+        Plot and save histogram or bar plot.
 
-        output:
-                None
-        '''
+        Parameters:
+        - data (pandas.Series): Data to plot.
+        - title (str): Title of the plot.
+        - save_name (str): File path to save the image.
+        - plot_type (str, optional): Type of plot ('hist' or 'bar'). Defaults to 'hist'.
+        """
+        fig, ax = plt.subplots(figsize=(20, 10))
+        if plot_type == 'hist':
+            ax.hist(data)
+        elif plot_type == 'bar':
+            data.value_counts().plot(kind='bar', ax=ax)
+        else:
+            raise ValueError("Invalid plot_type. Use 'hist' or 'bar'.")
+        ax.set_title(title)
+        ax.set_ylabel('Number of Customers')
+        self._save_plot(fig, save_name)
+    
+    def _plot_density(self, data, title, save_name):
+        """
+        Plot and save density plot.
+
+        Parameters:
+        - data (pandas.Series): Data to plot.
+        - title (str): Title of the plot.
+        - save_name (str): File path to save the image.
+        """
+        fig, ax = plt.subplots(figsize=(20, 10))
+        sns.histplot(data, stat='density', kde=True, ax=ax)
+        ax.set_title(title)
+        self._save_plot(fig, save_name)
+    
+    def _plot_heatmap(self, df, title, save_name):
+        """
+        Plot and save heatmap.
+
+        Parameters:
+        - df (pandas.DataFrame): Dataframe to plot.
+        - title (str): Title of the plot.
+        - save_name (str): File path to save the image.
+        """
+        fig, ax = plt.subplots(figsize=(20, 10))
+        sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths=2, ax=ax)
+        ax.set_title(title)
+        self._save_plot(fig, save_name)
+    
+    def perform_eda(self):
+        """
+        Perform exploratory data analysis (EDA) and save plots to images folder.
+        """
         df = self.df
-        # Plot and save churn vs. active histogram
-        self.plot_and_save_histogram(df['Attrition_Flag'], 
-                                'Churned vs Active Customer', 
-                                'images/churn_vs_active_histogram.png')
-        
-        # Plot and save customer age histogram
-        self.plot_and_save_histogram(df['Customer_Age'], 
-                                'Customer Age', 
-                                'images/customer_age_histogram.png')
-        
-        
+        self._plot_hist_or_bar(df['Attrition_Flag'], 
+                               'Churned vs Active Customer', 
+                               'images/churn_vs_active_histogram.png')
+        self._plot_hist_or_bar(df['Customer_Age'], 
+                               'Customer Age', 
+                               'images/customer_age_histogram.png')
+        self._plot_hist_or_bar(df['Marital_Status'], 
+                               'Marital Status', 
+                               'images/marital_status_barplot.png', 
+                               plot_type='bar')
+        self._plot_density(df['Total_Trans_Ct'], 
+                           'Total Transaction Count in The Last 12 Months', 
+                           'images/density_total_trans_count.png')
+        self._plot_heatmap(df, 
+                           'Heatmap Correlation', 
+                           'images/heatmap_corr.png'
 
     def encoder_helper(self, category_lst, response):
         '''
